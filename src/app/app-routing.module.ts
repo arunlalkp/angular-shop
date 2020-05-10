0c32359e0043dbff34eb2fe81b6f7c0efdfcbaef
Arun Lal <arunlalkp.kp@gmail.com>
2020-05-10 15:54:52 +0530

    product list displayed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,15 +23,19 @@ const routes: Routes = [
   {path:'check-out', component: CheckOutComponent, canActivate: [AuthGuard]},
   {path:'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]},
   {path:'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard]},
-  
-  {
-    path:'admin/products', 
-    component: AdminProductsComponent, 
-    canActivate: [AuthGuard, AdminAuthGuard]
-  },
   {
     path:'admin/products/new', 
     component: ProductFormComponent, 
+    canActivate: [AuthGuard, AdminAuthGuard]
+  },
+  {
+    path:'admin/products/:id', 
+    component: ProductFormComponent, 
+    canActivate: [AuthGuard, AdminAuthGuard]
+  },
+  {
+    path:'admin/products', 
+    component: AdminProductsComponent, 
     canActivate: [AuthGuard, AdminAuthGuard]
   },
   {
