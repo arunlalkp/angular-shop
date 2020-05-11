b5c7621c7f918d747c1b9d1269f430acd227da3b
Arun Lal <arunlalkp.kp@gmail.com>
2020-05-11 11:32:15 +0530

    filter products by category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import { switchMap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-products',
@@ -8,14 +11,33 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent  {
-  products$;
+  products:Product[] = [];
+  filteredProducts:Product[] = []
   categories$;
+  category;
 
   constructor(
     private productService:ProductService,
-    private categoryService:CategoryService) { 
-    this.products$ = productService.getAll()
+    private categoryService:CategoryService,
+    private route: ActivatedRoute) { 
+    productService.getAll()
+    .pipe(
+      switchMap(product => {
+        this.products = product
+        return this.route.queryParamMap
+      })
+    ).subscribe(params => {
+      this.category = params.get('category')
+  
+      this.filteredProducts = this.category ? 
+        this.products.filter(p => p.category == this.category) :
+        this.products;
+    })
+
     this.categories$ = categoryService.getAll()
+
+    
+
   }
 
  
