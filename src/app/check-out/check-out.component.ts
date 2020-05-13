c156308d9bbe55fa61113280d3c6fcfe4c9296ea
Arun Lal <arunlalkp.kp@gmail.com>
2020-05-13 16:43:36 +0530

    created checkout form

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckOutComponent implements OnInit {
 
+  shipping = {
+    name:'',
+    city:'',
+    addressLine1:'',
+    addressLine2:''
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  placeOrder(){
+    console.log(this.shipping)
   }
 
 }
