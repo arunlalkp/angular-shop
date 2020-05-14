c5304e4ecc407581ceacaa5ca256dfd9c139989a
Arun Lal <arunlalkp.kp@gmail.com>
2020-05-14 16:00:36 +0530

    created shared module and configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-//firebase
-import { AngularFireModule } from '@angular/fire'
-import { AngularFireDatabaseModule } from '@angular/fire/database'
-import { AngularFireAuthModule } from '@angular/fire/auth'
-
-import { AppRoutingModule, routingComponents } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CustomFormsModule } from 'ng2-validation';
+import { environment } from 'src/environments/environment';
+
+import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductCardComponent } from './shared/components/product-card/product-card.component';
-import { ProductQuantityComponent } from './shared/components/product-quantity/product-quantity.component';
+import { ShippingFormComponent } from './shipping-form/shipping-form.component';
 import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
-import { ShippingFormComponent } from './shipping-form/shipping-form.component'
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -28,13 +24,13 @@ import { ShippingFormComponent } from './shipping-form/shipping-form.component'
     routingComponents,
     ProductFormComponent,
     ProductFilterComponent,
-    ProductCardComponent,
-    ProductQuantityComponent,
+    
     ShoppingCartSummaryComponent,
     ShippingFormComponent
 
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
