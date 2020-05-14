2d329c23874609f1027452930dbda9f6c1a1af93
Arun Lal <arunlalkp.kp@gmail.com>
2020-05-14 11:54:52 +0530

    split checkout form to new comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { CustomFormsModule } from 'ng2-validation';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
-import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component'
+import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
+import { ShippingFormComponent } from './shipping-form/shipping-form.component'
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-c
     ProductFilterComponent,
     ProductCardComponent,
     ProductQuantityComponent,
-    ShoppingCartSummaryComponent
+    ShoppingCartSummaryComponent,
+    ShippingFormComponent
 
   ],
   imports: [
