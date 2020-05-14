634da9bb2efac5c1ac3b4440c94e1711b034cb32
Arun Lal <arunlalkp.kp@gmail.com>
2020-05-14 10:03:01 +0530

    created order service and store order data on firebase

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
   shipping = {
     name:'',
@@ -13,13 +17,43 @@ export class CheckOutComponent implements OnInit {
     addressLine1:'',
     addressLine2:''
   }
-  constructor() { }
+  subscription:Subscription
+  cart:ShoppingCart
 
-  ngOnInit(): void {
+
+  constructor( 
+    private orderService:OrderService,
+    private shoppingCartService:ShoppingCartService) { }
+
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart()
+    this.subscription = cart$.subscribe(cart => this.cart = cart)
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 
   placeOrder(){
     console.log(this.shipping)
+    let order = {
+      datePlace: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart._items.map(i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+
+    }
+
+    this.orderService.storeOrder(order);
   }
 
 }
